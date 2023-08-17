50f10459fb9571a6d79de0e38ea8fe1afbc05f29
JohnzExor <jimenojohnzyll@gmail.com>
2023-08-17 11:43:31 +0800

    fixed some bugs

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,6 @@
 const NavBar = () => {
   return (
-    <div className="bg-black text-white shadow-lg">
+    <div className="bg-black text-white shadow-lg fixed w-full">
       <div className="flex gap-4 justify-center p-4">
         <strong>PORTFOLIO</strong>
         <ul className="flex gap-4">
